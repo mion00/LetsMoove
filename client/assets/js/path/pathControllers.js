bcc7e380caea29d3b833fda9e17a85d9e983464b
M4rc0SX <federicimarcosx@gmail.com>
2015-11-19 14:16:56 +0100

    Syntax bug fix

--- client/assets/js/path/pathControllers.js
+++ client/assets/js/path/pathControllers.js
@@ -1,11 +1,23 @@
 /**
  * Created by mion00 on 18/11/15.
  */
+(function () {
+    var app = angular.module('pathControllers', ['pathServices']);
 
-var pathControllers = angular.module('pathControllers', ['pathServices']);
+    app.controller('pathListController', ['$scope', 'Path', function ($scope, Path) {
+        $scope.result = Path.query({where: {lastname : "doe"}});
+    }]);
 
-pathControllers.controller('pathListController', ['$scope', 'Path', function ($scope, Path) {
-    $scope.result = Path.query({where: {lastname : "doe"}});
-}]
+    app.contrller('pathDetailsController',['$stateProvider','Path',function($stateProvider,Path){
+        var callback = function(){
+            console.log("DONE");
+        };
 
-);
+        var error = function(){
+            console.log("FAIL");
+        };
+
+        this.path=Path.query({pathId:$stateProvider.pathId},callback,error);
+    }]);
+
+}());
