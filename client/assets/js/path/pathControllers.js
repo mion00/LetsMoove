edd29a6b90f692594cfee62cd304f8d1d8661936
M4rc0SX <federicimarcosx@gmail.com>
2015-12-02 15:16:55 +0100

    Aggiornata funzionalità di inserimento percorsi

--- client/assets/js/path/pathControllers.js
+++ client/assets/js/path/pathControllers.js
@@ -147,7 +147,12 @@
                         latitude: scope.paths[i].locationData.startPoint.coordinates[1],
                         longitude: scope.paths[i].locationData.startPoint.coordinates[0],
                         title: scope.paths[i].name,
-                        options: {labelClass: 'marker_labels', labelAnchor: '10 60', labelContent: scope.paths[i].name}
+                        options: {
+                            labelClass: 'marker_labels',
+                            labelAnchor: '10 60',
+                            labelContent: scope.paths[i].name,
+                            animation: google.maps.Animation.DROP
+                        }
                     });
             }
         }
@@ -206,7 +211,7 @@
 
     }]);
 
-    app.controller('pathInsertionController', ['Path', 'TerrainType', 'uiGmapGoogleMapApi', function (Path, TerrainType, uiGmapGoogleMapApi) {
+    app.controller('pathInsertionController', ['Path', 'TerrainType', 'uiGmapGoogleMapApi', '$scope', function (Path, TerrainType, uiGmapGoogleMapApi, $scope) {
         var scope = this;
 
         this.path = {
@@ -227,21 +232,16 @@
             },
             locationData: {
                 startPoint: {},
-                stages: [
-                    {
-                        location: {
-                            type: "Point",
-                            coordinates: []
-                        },
-                        question: "",
-                        answer: ""
-                    }
-                ]
+                stages: []
             }
 
         };
-
-        this.path.locationData.startPoint = this.path.locationData.stages[0].location;
+        this.zoom = 2;
+        this.center = {
+            type: "Point",
+            coordinates: [0, 20]
+        };
+        this.markers = [];
 
         this.terrainTypes = {};
         TerrainType.get({}, function (terrainTypes) {
@@ -261,15 +261,28 @@
             scope.geocoder = new google.maps.Geocoder();
             scope.distanceMatrix = new google.maps.DistanceMatrixService();
             scope.elevator = new google.maps.ElevationService;
+            scope.updateMarkers(); //to remove
             scope.computePathData();
+            scope.pathIcon = [{
+                icon: {
+                    path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
+                },
+            }];
         });
 
-        this.computePathData = function () {
-            var stages = [
+        this.updateMarkers = function () {
+            var stages = [{
+                location: {
+                    type: "Point",
+                    coordinates: [12, 46]
+                },
+                question: "",
+                answer: ""
+            },
                 {
                     location: {
                         type: "Point",
-                        coordinates: [12,46]
+                        coordinates: [12, 46.5]
                     },
                     question: "",
                     answer: ""
@@ -277,27 +290,44 @@
                 {
                     location: {
                         type: "Point",
-                        coordinates: [12,46.5]
+                        coordinates: [12, 46.7]
                     },
                     question: "",
                     answer: ""
-                },
-                {
-                    location: {
-                        type: "Point",
-                        coordinates: [12,46.7]
-                    },
-                    question: "",
-                    answer: ""
-                }
+                }];
+            for (var i = 0; i < stages.length; i++) {
+                scope.markers.push(
+                    {
+                        id: i,
+                        latitude: stages[i].location.coordinates[1],
+                        longitude: stages[i].location.coordinates[0],
+                        title: (i + 1),
+                        options: {
+                            label: (""+(i+1)),
+                            draggable: true,
+                        }
+                    });
+            }
+        }; //to remove
 
-            ]; // to update
-            scope.path.length = 0;
-            scope.path.time = 0;
-            scope.path.altitude = 0;
-            for(var i=0;i<stages.length-1;i++){
-                var origin = {lat:stages[i].location.coordinates[1],lng:stages[i].location.coordinates[0]};
-                var destination = {lat:stages[i+1].location.coordinates[1],lng:stages[i+1].location.coordinates[0]};
+        this.updateStagesFromMarkers = function () {
+            for (var i = 0; i < scope.markers; i++) {
+                scope.path.locationData.stages[i].location = {
+                    type: "Point",
+                    coordinates: [scope.markers[i].longitude, scope.markers[i].latitude]
+                };
+            }
+            this.path.locationData.startPoint = this.path.locationData.stages[0].location;
+        };
+
+        this.computePathData = function () {
+            var stages = scope.markers; // to update
+            scope.path.pathData.length = 0;
+            scope.path.pathData.time = 0;
+
+            for (var i = 0; i < stages.length - 1; i++) {
+                var origin = {lat: stages[i].latitude, lng: stages[i].longitude};
+                var destination = {lat: stages[i + 1].latitude, lng: stages[i + 1].longitude};
 
                 scope.distanceMatrix.getDistanceMatrix({
                     origins: [origin],
@@ -308,32 +338,35 @@
                     if (status !== google.maps.DistanceMatrixStatus.OK) {
                         alert('Error was: ' + status);
                     } else {
-                        if(response.rows.length==1){
+                        if (response.rows.length == 1) {
                             console.log(response.rows[0].elements[0]);
                             scope.path.pathData.length += response.rows[0].elements[0].distance.value;
-                            scope.path.pathData.time += Math.floor(response.rows[0].elements[0].duration.value/60);
+                            scope.path.pathData.time += Math.floor(response.rows[0].elements[0].duration.value / 60);
                         } else {
                             console.log("error");
                         }
-
+                        $scope.$apply();
                     }
                 });
+
             }
-            var minh=10000000,maxh=-10000000;
-            for(var i=0;i<stages.length;i++) {
-                var stage = {lat:stages[i].location.coordinates[1],lng:stages[i].location.coordinates[0]};
+            var minh = 10000000, maxh = -10000000;
+            scope.path.pathData.altitude = 0;
+            for (var i = 0; i < stages.length; i++) {
+                var stage = {lat: stages[i].latitude, lng: stages[i].longitude};
                 scope.elevator.getElevationForLocations({
                     'locations': [stage]
                 }, function (responce) {
                     console.log(responce[0].elevation);
-                    if(responce[0].elevation<minh){
-                        minh=responce[0].elevation
+                    if (responce[0].elevation < minh) {
+                        minh = responce[0].elevation
                     }
-                    if(responce[0].elevation>maxh){
-                        maxh=responce[0].elevation
+                    if (responce[0].elevation > maxh) {
+                        maxh = responce[0].elevation
                     }
-                    scope.path.pathData.altitude += Math.floor(responce[0].elevation/stages.length);
-                    scope.path.pathData.deltaAltitude = Math.floor(maxh-minh);
+                    console.log(Math.floor(responce[0].elevation / stages.length));
+                    scope.path.pathData.altitude += Math.floor(responce[0].elevation / stages.length);
+                    scope.path.pathData.deltaAltitude = Math.floor(maxh - minh);
                 });
             }
         };
